4f89057c2b55e0e4f37b5a2dc162ca5ab840169a
윤성연 <s2427@e-mirim.hs.kr>
2026-06-22 20:24:57 +0900

    save

--- db-connector.js
+++ db-connector.js
@@ -13,7 +13,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST ?? "localhost",
   user: process.env.DB_USER ?? "root",
   password: process.env.DB_PASS ?? "",
-  database: process.env.DB_NAME ?? "photos",
+  database: process.env.DB_NAME ?? "railway",
   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
   waitForConnections: true,
   connectionLimit: Number(process.env.DB_CONN_LIMIT ?? 10),
